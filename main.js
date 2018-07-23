793964b88e83f8ca24970daa1ccc1d6e30881892
gretchenmarie <gretchenmward@gmail.com>
2018-07-23 11:57:12 -0500

    added initial js,html,css files

---
+++ main.js
@@ -0,0 +1,27 @@
+const financialorg = object.create({}, {
+     companyName:{
+         value: "finance company",
+         writeable: true,
+         enumerable: true
+     },
+     companySpecialty: {
+         value: "financial planning",
+         writeable: true,
+         enumerable: true,
+
+     },
+
+      customerName: {
+          Name: "Gretchen",
+          enumerable: true,
+      },
+      financialportfolia: {
+          value: "Target, Apple, Dell",
+          enumerable: false,
+
+      },
+      
+
+
+
+}
